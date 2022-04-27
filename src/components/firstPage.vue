78e9f696247eb73ae61b19a7148d82253ac16a49
Maksim <maksim.karpuk13910@gmail.com>
2022-04-27 21:00:20 +0300

    feature/TD-1

--- src/components/firstPage.vue
+++ src/components/firstPage.vue
@@ -1,11 +1,43 @@
 <template>
   <div class="content">
     <div class="folder">
-      <img src="./src" class="folder" />
+      <img src="../assets/images/folder.png" class="folder" />
     </div>
     <div class="board">
       <div class="boardHeader">to do list</div>
-      <div class="boaderContent"></div>
+      <div class="boardMain">
+        <div class="boaderContent">
+          <form action="" class="boaderContentForm">
+            <input type="checkbox" class="boaderContentFormCheckbox" />
+          </form>
+          <div class="boaderContentFormText">Task 1</div>
+          <button class="boaderContentFormButton">x</button>
+        </div>
+        <div class="boaderContent">
+          <form action="" class="boaderContentForm">
+            <input type="checkbox" class="boaderContentFormCheckbox" />
+          </form>
+          <div class="boaderContentFormText">Task 1</div>
+          <button class="boaderContentFormButton">x</button>
+        </div>
+        <div class="boaderContent">
+          <form action="" class="boaderContentForm">
+            <input type="checkbox" class="boaderContentFormCheckbox" />
+          </form>
+          <div class="boaderContentFormText">Task 1</div>
+          <button class="boaderContentFormButton">x</button>
+        </div>
+      </div>
+      <div class="boardFooter">
+        <div class="boardFooterText">1/3 left</div>
+        <div class="boardFooterOptions">
+          <form action="" class="boardFooterOptionsForm">
+            <input type="radio" class="boardFooterOptionsFormRadio" />
+            <input type="radio" class="boardFooterOptionsFormRadio" />
+            <input type="radio" class="boardFooterOptionsFormRadio" />
+          </form>
+        </div>
+      </div>
     </div>
   </div>
 </template>
