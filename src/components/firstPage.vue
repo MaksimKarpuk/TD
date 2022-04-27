f95f0b2b6edc6b13141faca012a6745dfa31d3be
Maksim <maksim.karpuk13910@gmail.com>
2022-04-27 22:50:47 +0300

    feature/TD-1

--- src/components/firstPage.vue
+++ src/components/firstPage.vue
@@ -1,31 +1,46 @@
 <template>
   <div class="content">
     <div class="folder">
-      <img src="../assets/images/folder.png" class="folder" />
+      <img src="../assets/images/folder.png" class="folderImage" />
     </div>
     <div class="board">
       <div class="boardHeader">to do list</div>
       <div class="boardMain">
-        <div class="boaderContent">
-          <form action="" class="boaderContentForm">
-            <input type="checkbox" class="boaderContentFormCheckbox" />
-          </form>
-          <div class="boaderContentFormText">Task 1</div>
-          <button class="boaderContentFormButton">x</button>
+        <div class="boaderMainContent">
+          <div class="boaderMainContentInfo">
+            <form action="" class="boaderMainContentInfoForm">
+              <input
+                type="checkbox"
+                class="boaderMainContentInfoFormCheckbox"
+              />
+            </form>
+            <div class="boaderMainContentInfoText">Task 1</div>
+          </div>
+          <button class="boaderMainContentButton">x</button>
         </div>
-        <div class="boaderContent">
-          <form action="" class="boaderContentForm">
-            <input type="checkbox" class="boaderContentFormCheckbox" />
-          </form>
-          <div class="boaderContentFormText">Task 1</div>
-          <button class="boaderContentFormButton">x</button>
+        <div class="boaderMainContent">
+          <div class="boaderMainContentInfo">
+            <form action="" class="boaderMainContentInfoForm">
+              <input
+                type="checkbox"
+                class="boaderMainContentInfoFormCheckbox"
+              />
+            </form>
+            <div class="boaderMainContentInfoText">Task 2</div>
+          </div>
+          <button class="boaderMainContentButton">x</button>
         </div>
-        <div class="boaderContent">
-          <form action="" class="boaderContentForm">
-            <input type="checkbox" class="boaderContentFormCheckbox" />
-          </form>
-          <div class="boaderContentFormText">Task 1</div>
-          <button class="boaderContentFormButton">x</button>
+        <div class="boaderMainContent">
+          <div class="boaderMainContentInfo">
+            <form action="" class="boaderMainContentInfoForm">
+              <input
+                type="checkbox"
+                class="boaderMainContentInfoFormCheckbox"
+              />
+            </form>
+            <div class="boaderMainContentInfoText">Task 3</div>
+          </div>
+          <button class="boaderMainContentButton">x</button>
         </div>
       </div>
       <div class="boardFooter">
