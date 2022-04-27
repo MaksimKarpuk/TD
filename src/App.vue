08fc0a47e7c495dc830aba74a7bde1334206cd76
Maksim <maksim.karpuk13910@gmail.com>
2022-04-27 16:28:36 +0300

    main

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+    <FirstPage />
+  </div>
+</template>
+
+<script>
+import FirstPage from "@/components/firstPage.vue";
+
+export default {
+  name: "App",
+  components: {
+    FirstPage,
+  },
+};
+</script>
+
+<style lang="scss">
+@import "@/assets/styles/style.scss";
+@import "@/assets/styles/folder.scss";
+</style>
---
+++ src/components/firstPage.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="content">
+    <div class="folder">
+      <img src="./src" class="folder" />
+    </div>
+    <div class="board">
+      <div class="boardHeader">to do list</div>
+      <div class="boaderContent"></div>
+    </div>
+  </div>
+</template>
